380880b5b4258c2823d3f0cdedae5d2c448bd7b7
Anthony Desormeau <anthony.desormeau@gmail.com>
2017-01-09 20:27:24 +0000

    initial commit

---
+++ math.js
@@ -0,0 +1,11 @@
+function multiply(a, b) {
+    if (a === 1) {
+        return b;
+    }
+    else if (a === 2) {
+        return b + b;
+    }
+    else {
+        return "i can't do math that complex";
+    }
+}
